ecb85bdcc909be3718f77ee6ad16038e33db9155
Karan Kumar <officialkaran39@gmail.com>
2025-02-23 22:26:49 +0530

    [Karan]Add. extended greeting controller to use service layer to get simple greeting message Hello world

--- greetingapp/src/main/java/com/spring/greetingapp/GreetingController.java
+++ greetingapp/src/main/java/com/spring/greetingapp/GreetingController.java
@@ -1,30 +1,38 @@
 package com.spring.greetingapp;
 
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-
+    private final GreetingService greetingService;
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
     @GetMapping
-    public Map<String, String> getGreeting() {
-        return Map.of("message", "Hello World");
+    public Map<String, String> getGreeting(@RequestParam(required = false) String firstName,
+                                           @RequestParam(required = false) String lastName) {
+        String message = greetingService.getGreetingMessage(firstName, lastName);
+        return Map.of("message", message);
     }
-
-    @PostMapping
-    public Map<String, String> postGreeting() {
-        return Map.of("message", "POST request received");
-    }
-
-    @PutMapping
-    public Map<String, String> putGreeting() {
-        return Map.of("message", "PUT request received");
-    }
-
-    @DeleteMapping
-    public Map<String, String> deleteGreeting() {
-        return Map.of("message", "DELETE request received");
-    }
+//    @GetMapping
+//    public Map<String, String> getGreeting() {
+//        return Map.of("message", "Hello World");
+//    }
+//
+//    @PostMapping
+//    public Map<String, String> postGreeting() {
+//        return Map.of("message", "POST request received");
+//    }
+//
+//    @PutMapping
+//    public Map<String, String> putGreeting() {
+//        return Map.of("message", "PUT request received");
+//    }
+//
+//    @DeleteMapping
+//    public Map<String, String> deleteGreeting() {
+//        return Map.of("message", "DELETE request received");
+//    }
 }
